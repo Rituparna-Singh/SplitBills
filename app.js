9f5075fc3503bfd134bfdea5bee8a4ab40972b65
Rituparna Singh <96687610+Rituparna-Singh@users.noreply.github.com>
2024-01-11 11:52:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// function convert(){
+//     let cm = document.getElementById('cm');
+//     let m = cm.value * 100;
+    
+//     let result = "Converted value: " +m+"cm";
+//     document.getElementById('result').innerText = result;
+// }
+
+function split(){
+    let amt = document.getElementById('amount');
+    let per = document.getElementById('person');
+
+    let result = (amt.value / per.value).toFixed(2);
+    document.getElementById('results').innerText = "Each share will be " + result;
+    console.log(result);
+
+    amt.value ='';
+    per.value ='';
+}
